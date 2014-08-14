54ac7b7185d9d71da96d66a6541b931b64bfa180
Wolf480pl <wolf480@interia.pl>
2014-08-14 10:31:47 +0200

    Implement the PluginManager

--- src/main/java/com/flowpowered/plugins/PluginManager.java
+++ src/main/java/com/flowpowered/plugins/PluginManager.java
@@ -1,24 +1,67 @@
 package com.flowpowered.plugins;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PluginManager {
+    private final List<PluginLoader> loaders = new LinkedList<>(); // TODO: Make this a set and make PluginLoaders hashset/map compatible?
+    private final Map<String, PluginHandle> plugins = new HashMap<>();
+    private final Logger logger;
+    private final String pluginLoggerPrefix;
 
-    public PluginManager() {
-        // TODO Auto-generated constructor stub
+    public PluginManager(Logger logger) {
+        this.logger = logger;
+        this.pluginLoggerPrefix = logger.getName() + ".";
     }
 
     public void enable(PluginHandle plugin) {
-        // TODO
+        if (plugin.getManager() != this) {
+            throw new IllegalArgumentException("Not our plugin");
+        }
+        // TODO: thread safety
+        if (plugin.getState() != PluginState.DISABLED) {
+            // TODO: don't fail silently
+            return;
+        }
+        plugin.setState(PluginState.ENABLING);
+        plugin.onEnable(); // TODO: Add a way for it to fail
+        plugin.setState(PluginState.ENABLED);
     }
 
     public void disable(PluginHandle plugin) {
-        // TODO
+        if (plugin.getManager() != this) {
+            throw new IllegalArgumentException("Not our plugin");
+        }
+        // TODO: thread safety
+        if (plugin.getState() != PluginState.ENABLED) {
+            // TODO: don't fail silently
+            return;
+        }
+        plugin.setState(PluginState.DISABLING);
+        plugin.onDisable(); // TODO: Add a way for it to fail
+        plugin.setState(PluginState.DISABLED);
+    }
+
+    public void addLoader(PluginLoader loader) {
+        loaders.add(loader);
+        /* TODO: Don't load all of them like this:
+         * - this overrides existing ones with the same name
+         * - this loads more than needed, should be more lazy
+         * - this scans for them only once and never checks again
+         */
+        plugins.putAll(loader.loadAll(this));
     }
 
     public Logger getLogger(PluginHandle plugin) {
-        // TODO
-        return null;
+        return LoggerFactory.getLogger(pluginLoggerPrefix + plugin.getName());
     }
 
+    public PluginHandle getPluginHandle(String name) {
+        return plugins.get(name);
+    }
 }
---
+++ src/main/java/com/flowpowered/plugins/PluginState.java
@@ -0,0 +1,5 @@
+package com.flowpowered.plugins;
+
+public enum PluginState {
+    DISABLED, ENABLING, ENABLED, DISABLING
+}
